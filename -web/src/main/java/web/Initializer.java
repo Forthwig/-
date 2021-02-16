0868a4ae07fe6be8f7f02bf4b5630bbcf274b930
MatD3mons <matthieu.desmarescaux@isen.yncrea.fr>
2021-02-16 15:55:47 +0100

    structure du projet

---
+++ -web/src/main/java/web/Initializer.java
@@ -0,0 +1,42 @@
+package web;
+
+import junia.lab06.core.config.AppConfig;
+import junia.lab06.core.config.DBConfig;
+import org.apache.cxf.transport.servlet.CXFServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import web.config.WSConfig;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+
+//TODO complete the implementation of this class. Take a look at lab05 !
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        Dynamic servlet = servletContext.addServlet("cxfServlet", new CXFServlet());
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/api/*");
+    }
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { AppConfig.class, DBConfig.class, WSConfig.class};
+    }
+
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] {};
+    }
+
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/web/" };
+    }
+}
